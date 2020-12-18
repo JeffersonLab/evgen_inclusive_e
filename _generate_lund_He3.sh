d5b828b9dfd05511af7e8860dafab96816e326e1
tianye8001 <tianye8001@gmail.com>
2020-12-18 15:06:24 -0500

    modify some directories

--- _generate_lund_He3.sh
+++ _generate_lund_He3.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh -f 
-source /w/halla-scifs17exp/solid/tianye/solid_simulation/solid/evgen/evgen_inclusive_e-dflay_dev-4/setup
- /w/halla-scifs17exp/solid/tianye/solid_simulation/solid/evgen/evgen_inclusive_e-dflay_dev-4/solid_inclusive_e /w/halla-scifs17exp/solid/tianye/solid_simulation/solid/evgen/evgen_inclusive_e-dflay_dev-4/input/parameters_d2n.txt
+source /w/halla-scifs17exp/solid/tianye/solid_simulation/evgen_inclusive_e/setup
+ /w/halla-scifs17exp/solid/tianye/solid_simulation/evgen_inclusive_e/solid_inclusive_e /w/halla-scifs17exp/solid/tianye/solid_simulation/evgen_inclusive_e/input/parameters_d2n.txt
   mv gen_d2n.root ./gen_d2n_fileN.root
   rm gen_d2n.lund
 
