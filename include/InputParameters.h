06503dd610405b984b8045c1feb5eec506dd9fe5
tianye8001 <tianye8001@gmail.com>
2020-12-23 17:09:58 -0500

    add radiation length Tb and Ta as input parameters

--- include/InputParameters.h
+++ include/InputParameters.h
@@ -28,13 +28,16 @@ typedef struct inputParameters {
    int tgt_Z;                     // target Z: number of protons       
    int tgt_A;                     // target A: atomic mass (= Z + N)
    int scale;                     // 0: no scale factor, 1:apply a scale factor on the cross sections
+   int rad;                     // 0: Born cross section, 1:radiative cross section to get rate
+   double RLb;                     //Total radiation length X0 before vertex 
+   double RLa;                     //Total radiation length X0 after vertex 
 
    // constructor 
    inputParameters(): 
       output_name("NONE"),pol_pdfset_name("NONE"),pol_pdfset_ID("0000"),unpol_pdfset_name("NONE"),unpol_pdfset_ID("0000"),
       lumi(0),E_beam(0),theta_min(0),theta_max(0),Ep_min(0),Ep_max(0),
       vx_min(0),vx_max(0),vy_min(0),vy_max(0),vz_min(0),vz_max(0),
-      num_evt(0),tgt_Z(0),tgt_A(0),scale(0) 
+      num_evt(0),tgt_Z(0),tgt_A(0),scale(0),rad(0),RLb(0),RLa(0)
    {}
 
 } inputParameters_t; 
